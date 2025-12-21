5972b0264bfcb096f2fb4c6272e854c1e180c8a2
bruno57310 <bruno_wendling@orange.fr>
2025-12-21 14:40:12 +0100

    Update from Bolt.diy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import './i18n';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,       // Enable new transition behavior
+        v7_relativeSplatPath: true      // Enable new splat path resolution
+      }}
+    >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
